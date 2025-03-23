7b077137ffcfa911589ba7f10d8cbb94cd8f21b4
unifolio0 <boho82ha@gmail.com>
2025-03-23 18:31:24 +0900

    feat: 메인 로직 구현

--- EmptyViewer/Main_EmptyViewer.cpp
+++ EmptyViewer/Main_EmptyViewer.cpp
@@ -17,89 +17,77 @@
 
 using namespace glm;
 
-// -------------------------------------------------
-// Global Variables
-// -------------------------------------------------
-int Width = 1280;
-int Height = 720;
+#include "Ray.h"
+#include "Vec3.h"
+#include "Camera.h"
+#include "Scene.h"
+#include "Sphere.h"
+#include "Plane.h"
+
+int Width = 512;
+int Height = 512;
+
 std::vector<float> OutputImage;
-// -------------------------------------------------
 
-
+GLuint texID;
 
 void render()
 {
-	//Create our image. We don't want to do this in 
-	//the main loop since this may be too slow and we 
-	//want a responsive display of our beautiful image.
-	//Instead we draw to another buffer and copy this to the 
-	//framebuffer using glDrawPixels(...) every refresh
-	OutputImage.clear();
-	for (int j = 0; j < Height; ++j) 
-	{
-		for (int i = 0; i < Width; ++i) 
-		{
-			// ---------------------------------------------------
-			// --- Implement your code here to generate the image
-			// ---------------------------------------------------
+    Camera camera(
+        Vec3(0, 0, 0),
+        Vec3(1, 0, 0),
+        Vec3(0, 1, 0),
+        Vec3(0, 0, 1),
+        -0.1f, 0.1f,
+        -0.1f, 0.1f,
+        0.1f,
+        Width, Height
+    );
 
-			// draw a red rectangle in the center of the image
-			vec3 color = glm::vec3(0.5f, 0.5f, 0.5f); // grey color [0,1] in RGB channel
-			
-			if (i > Width / 4 && i < 3 * Width / 4 
-				&& j > Height / 4 && j < 3 * Height / 4)
-			{
-				color = glm::vec3(1.0f, 0.0f, 0.0f); // red color [0,1] in RGB channel
-			}
-			
-			// set the color
-			OutputImage.push_back(color.x); // R
-			OutputImage.push_back(color.y); // G
-			OutputImage.push_back(color.z); // B
-		}
-	}
+    Scene scene;
+    scene.addObject(new Sphere(Vec3(-4, 0, -7), 1));
+    scene.addObject(new Sphere(Vec3(0, 0, -7), 2));
+    scene.addObject(new Sphere(Vec3(4, 0, -7), 1));
+    scene.addObject(new Plane(-2));
+
+    for (int j = 0; j < Height; ++j) {
+        for (int i = 0; i < Width; ++i) {
+            Ray ray = camera.generateRay(i, j);
+            if (scene.closestIntersection(ray)) {
+                OutputImage.push_back(255);
+                OutputImage.push_back(255);
+                OutputImage.push_back(255);
+            }
+            else {
+                OutputImage.push_back(0);
+                OutputImage.push_back(0);
+                OutputImage.push_back(0);
+            }
+        }
+    }
 }
 
-
-void resize_callback(GLFWwindow*, int nw, int nh) 
+void resize_callback(GLFWwindow*, int nw, int nh)
 {
-	//This is called in response to the window resizing.
-	//The new width and height are passed in so we make 
-	//any necessary changes:
-	Width = nw;
-	Height = nh;
-	//Tell the viewport to use all of our screen estate
-	glViewport(0, 0, nw, nh);
+    Width = nw;
+    Height = nh;
+    glViewport(0, 0, nw, nh);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
 
-	//This is not necessary, we're just working in 2d so
-	//why not let our spaces reflect it?
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-
-	glOrtho(0.0, static_cast<double>(Width)
-		, 0.0, static_cast<double>(Height)
-		, 1.0, -1.0);
-
-	//Reserve memory for our render so that we don't do 
-	//excessive allocations and render the image
-	OutputImage.reserve(Width * Height * 3);
-	render();
+    glOrtho(0.0, static_cast<double>(Width)
+        , 0.0, static_cast<double>(Height)
+        , 1.0, -1.0);
+    OutputImage.reserve(Width * Height * 3);
+    render();
 }
 
-
-int main(int argc, char* argv[])
-{
-	// -------------------------------------------------
-	// Initialize Window
-	// -------------------------------------------------
-
+int main(int argc, char* argv[]) {
 	GLFWwindow* window;
 
-	/* Initialize the library */
 	if (!glfwInit())
 		return -1;
 
-	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(Width, Height, "OpenGL Viewer", NULL, NULL);
 	if (!window)
 	{
@@ -107,42 +95,24 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 
-	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
-	//We have an opengl context now. Everything from here on out 
-	//is just managing our window or opengl directly.
-
-	//Tell the opengl state machine we don't want it to make 
-	//any assumptions about how pixels are aligned in memory 
-	//during transfers between host and device (like glDrawPixels(...) )
 	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 	glPixelStorei(GL_PACK_ALIGNMENT, 1);
 
-	//We call our resize function once to set everything up initially
-	//after registering it as a callback with glfw
 	glfwSetFramebufferSizeCallback(window, resize_callback);
 	resize_callback(NULL, Width, Height);
 
-	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
-		//Clear the screen
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// -------------------------------------------------------------
-		//Rendering begins!
 		glDrawPixels(Width, Height, GL_RGB, GL_FLOAT, &OutputImage[0]);
-		//and ends.
-		// -------------------------------------------------------------
-
-		/* Swap front and back buffers */
+		
 		glfwSwapBuffers(window);
 
-		/* Poll for and process events */
 		glfwPollEvents();
 
-		//Close when the user hits 'q' or escape
 		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS
 			|| glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
 		{
